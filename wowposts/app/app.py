6082e5436b01340fcf6268a6e332d5455dc53975
nd <ndndnd@ura.ura>
2025-08-13 16:55:45 +0300

    Намечание структуры проекта и кода, добавление 75% основных маршрутов, без функционала, просто декоратор+функция+плюс текстовый ретюрн. Следующим делом необходимо дописать маршруты, наметить модели для БД, наметить базовые шаблоны и так далее.

--- wowposts/app/app.py
+++ wowposts/app/app.py
@@ -1,1 +1,83 @@
-2
+# импорт необходимых модулей
+from flask import Flask, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# создание экземпляра БД
+db = SQLAlchemy()
+# создание экземпляра приложения
+app = Flask(__name__)
+
+# МАРШРУТЫ ДЛЯ РАБОТЫ С ЛОГИНОМ РЕГИСТРАЦИЕЙ И ТД
+# маршрут логина
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'POST-method'
+    else:
+        return 'login-form'
+
+# маршрут логаута
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+# маршрут регистрации
+@app.route('/register')
+def register():
+    return 'register-form'
+#====#====#====#
+# БАЗОВЫЕ МАРШРУТЫ
+# основной маршрут
+@app.route('/')
+def home():
+    return 'home'
+
+# маршрут "о нас"
+@app.route('/about')
+def about():
+    return 'about us'
+#====#====#====#
+# МАРШРУТЫ РАБОТЫ С ПРОФИЛЕМ
+# маршрут профиля пользователя
+@app.route('/user/<username>')# НУЖНО СДЕЛАТЬ ЧТОБЫ ПОЛЬЗОВАТЕЛЬ МОГ
+# РЕДАКТИРОВАТЬ ПРОФИЛЬ
+def show_user_profile(username):
+    return f'user: {username}'
+#====#====#====#
+# МАРШРУТЫ РАБОТЫ С ПОСТАМИ
+# маршрут создания поста
+@app.route('/post/create')
+def create_post():
+    return 'create post'
+
+# маршрут детализации поста
+@app.route('/post/<int:post_id>')
+def detail_post(post_id):
+    return f'post #{post_id}'
+
+# маршрут редактирования поста
+@app.route('/post/<int:post_id>/edit')
+def edit_post(post_id):
+    return f'rditing post #{post_id}'
+#====#====#====#
+# МАРШРУТЫ РАБОТЫ С КОММЕНТАРИЯМИ
+# маршрут добавления комментария
+@app.route('/post/<int:post_id>/comment')
+def add_comment(post_id):
+    return f'add comment to post #{post_id}'
+
+# маршрут редактирования комментария
+@app.route('/comment/<int:comment_id>/edit')
+def edit_comment(comment_id):
+    return f'editing comment #{comment_id}'
+
+# маршрут удаления комментария
+@app.route('/comment/<int:comment_id>/delete')
+def delete_comment(comment_id):
+    return f'deleting comment #{comment_id}'
+# маршрут удаления поста
+# -------------------------
+
+# запуск приложения
+if __name__ == '__main__':
+    app.run()
--- wowposts/app/config.py
+++ wowposts/app/config.py
@@ -1,1 +1,15 @@
-2
+# импорт класса Env из библиотеки environs для работы
+# с .env файлами (переменными окружения)
+from environs import Env
+# инициализация объекта класса Env()
+env = Env()
+# чтение переменных окружения
+env.read_env()
+
+# создание класса конфига
+class Config:
+    def __init__(self):
+        # читает секретный ключ для шифрования трафика
+        self.secret_key = env('SECRET_KEY')
+        # читает строку подключения к БД
+        self.database_uri = env('DATABASE_URI')
